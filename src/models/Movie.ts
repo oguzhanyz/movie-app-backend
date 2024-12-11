9d2964a6cf202071f2282ea2905f05b1259902e8
oguzhanyz <oguzhanyz74@gmail.com>
2024-12-11 17:40:18 +0300

    Add movie model

---
+++ src/models/Movie.ts
@@ -0,0 +1,27 @@
+import { Schema, model, Document } from "mongoose";
+
+interface IMovie extends Document {
+  tconst: string;
+  titleType: string;
+  primaryTitle: string;
+  originalTitle: string;
+  isAdult: boolean;
+  startYear: number;
+  endYear: number;
+  runtimeMinutes: number;
+  genres: string[];
+}
+
+const MovieSchema = new Schema<IMovie>({
+  tconst: { type: String, required: true, unique: true },
+  titleType: { type: String, required: true },
+  primaryTitle: { type: String, required: true },
+  originalTitle: { type: String, required: true },
+  isAdult: { type: Boolean, required: true },
+  startYear: { type: Number, required: true },
+  endYear: { type: Number, default: null },
+  runtimeMinutes: { type: Number, required: true },
+  genres: { type: [String], required: true },
+});
+
+export default model<IMovie>("Movie", MovieSchema);
